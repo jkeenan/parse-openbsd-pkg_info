ae3f7ec2f921fbc0ce07584e5302c95d70b529a2
James E Keenan <jkeenan@cpan.org>
2018-01-19 09:43:47 -0500

    Change from OO to functional interface.
    Define and test sub L() for 'pkg_add -L <package1> <package2> ...'.
    
    TODO: Refine test so that we use mock input rather than live calls.
    We'll only use live calls for non-existent package.

--- lib/Parse/OpenBSD/pkg_info.pm
+++ lib/Parse/OpenBSD/pkg_info.pm
@@ -2,17 +2,69 @@ package Parse::OpenBSD::pkg_info;
 use 5.14.0;
 use warnings;
 our $VERSION = '0.01';
+use parent qw( Exporter );
+our @EXPORT_OK= qw( L );
 use Carp;
+use Data::Dump qw( dd pp );
 
+sub L {
+    my @pkgs = @_;
+    my $pkg_info_L_output = `pkg_info -L @pkgs`;
+    my $results = [ split(/\n{3,}/, $pkg_info_L_output) ];
+    my $found = _parse($results);
+    my %requests = map { $_ => 1 } @pkgs;
+    for my $req (keys %requests) {
+        $found->{$req} = undef unless exists $found->{$req};
+    }
+    return $found;
+}
 
-sub new {
-    my ($class, $params) = @_;
-    $params //= {};
-    croak "new(): Incorrect argument; must supply hash ref"
-        unless ref($params) eq 'HASH';
+sub _parse {
+    my $results = shift;
+    my %found = ();
+    for my $p (@{$results}) {
+        my @lines = split(/\n+/, $p);
+        if ($lines[0] =~ m/^Information for inst:((.*?)-([^\-]+))$/) {
+            my ($versioned_package, $package, $version) = ($1,$2,$3);
+            $found{$package}{installed} = 1;
+            $found{$package}{versioned_package} = $versioned_package;
+            $found{$package}{version} = $version;
+            $found{$package}{files} = _parse_result_for_files(@lines);
+        }
+        elsif ($lines[0] =~ m{^
+            Information\sfor\s
+            (
+                (?:http.*)/(
+                    (
+                        .*?
+                    )               # package
+                    -
+                    (
+                        [^\-]+
+                    )               # version
+                )                   # versioned_package
+                \.tgz
+            )                       # url
+        $}x) {
+            my ($url, $versioned_package, $package, $version) = ($1,$2,$3,$4);
+            $found{$package}{installed} = 0;
+            $found{$package}{url} = $url;
+            $found{$package}{versioned_package} = $versioned_package;
+            $found{$package}{version} = $version;
+            $found{$package}{files} = _parse_result_for_files(@lines);
+        }
+    }
+    return \%found;
+}
 
-    my $self = bless $params, $class;
-    return $self;
+sub _parse_result_for_files {
+    my @lines = @_;
+    my @files;
+    for my $l (@lines[1..$#lines]) {
+        next if $l =~ m/^(\s*|Files:)$/;
+        push @files, $l;
+    }
+    return \@files;
 }
 
 1;
--- t/001-load.t
+++ t/001-load.t
@@ -2,19 +2,23 @@
 
 # t/001-load.t - check module loading and create testing directory
 
-use Test::More tests =>  3;
+use Test::More tests =>  4;
+use Parse::OpenBSD::pkg_info qw( L );
+#use Data::Dump qw ( dd pp );
 
-BEGIN { use_ok( 'Parse::OpenBSD::pkg_info' ); }
+my (@requests, $results);
+my (@valid, @phony);
 
-my $self;
+@valid = qw( p5-Algorithm-Diff p5-Cairo );
+@phony = qw( p5-My-Foo-Bar );
+@requests = (@valid, @phony);
 
-$self = Parse::OpenBSD::pkg_info->new ();
-isa_ok ($self, 'Parse::OpenBSD::pkg_info');
-
-{
-    local $@;
-    eval { $self = Parse::OpenBSD::pkg_info->new([]); };
-    like $@, qr/new\(\): Incorrect argument; must supply hash ref/,
-        "new(): Got expected error message: wrong argument type";
+$results = L(@requests);
+#pp($results);
+is(keys %{$results}, 3, "Results reported for three requests");
+for my $req (@valid) {
+    ok(defined $results->{$req}, "'$req' found, as expected");
 }
+ok(! defined $results->{$phony[0]}, "'$phony[0]' not found, as expected");
+
 
